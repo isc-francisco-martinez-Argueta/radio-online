1dc76033af7c7b187120a4436ea424c1fc4b2a80
Francisco Martinez <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2024-05-18 23:55:35 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
 
-// Navegation Menu
-let btnMenu = document.querySelector('.btn-menu');
-let barIconX = document.querySelector('.btn-menu i');
-let menu = document.querySelector('.list-container');
 let menuContent = document.querySelector('.menu');
-var activador = true;
-
-
-
-
-
 //Scroll Efect
 
  let prevScrollPos = window.pageYOffset;
@@ -28,40 +18,30 @@ window.onscroll = () => {
     menuContent.style.transition = '0.5s';
   }
   prevScrollPos = currentScrollPos;
-   
 }
 
 
 
 
-alert('ds');
+
 
 const audio = document.getElementById("audio");
-  const playPause = document.getElementById("play");
-  const mute = document.getElementById("muted");
+const playPause = document.getElementById("play");
+const mute = document.getElementById("muted");
 
 playPause.addEventListener("click", () => {
   if (audio.paused || audio.ended) {
-    alert('hola');
     playPause.querySelector(".pause-btn").classList.toggle("hide");
     playPause.querySelector(".play-btn").classList.toggle("hide");
     background.querySelector(".play").classList.toggle("red");
     background.querySelector(".play").classList.toggle("blue");
     audio.play();
-
-    
-
   } else {
-
-
-    
     audio.pause();
     playPause.querySelector(".pause-btn").classList.toggle("hide");
     playPause.querySelector(".play-btn").classList.toggle("hide");
     background.querySelector(".play").classList.toggle("blue");
-    background.querySelector(".play").classList.toggle("red");
-
-    
+    background.querySelector(".play").classList.toggle("red");    
   }
   
   
