ce7fced6ef66f825b19bfffe68298dbf67ca30dc
Francisco Martinez <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2024-05-26 01:59:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+console.log('entra');
 if ('serviceWorker' in navigator) {
-  console.log('entra')
+  
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
