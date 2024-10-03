5264c65b008f08a8c5767105e85e660a8bb334fb
Francisco Martinez <48078304+isc-francisco-martinez-Argueta@users.noreply.github.com>
2024-10-02 19:40:04 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const audio = document.getElementById("audio");
 const playPause = document.getElementById("play");
 
 // Establecer la URL de la transmisión de radio
-const radioUrl = "https://fdsfdsfdsf.radio12345.com/intro.mp3";
+const radioUrl = "http://uk15freenew.listen2myradio.com:27678/;stream.mp3";
 audio.src = radioUrl;
 
 function togglePlayPauseIcons() {
